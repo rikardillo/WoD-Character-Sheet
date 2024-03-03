152d0b8e495fd16163f9714f4992524138979f7b
Ricardo Domínguez <ricardomdominguezr@gmail.com>
2024-03-02 22:19:53 -0400

    Added input component

--- src/components/Attribute.jsx
+++ src/components/Attribute.jsx
@@ -1,12 +1,65 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const StyledAttribute = styled.div``;
+const ListItem = ({ title = "name", entry = "name" }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedEntry, setEditedEntry] = useState(entry);
 
-export default function Attribute({ title, input }) {
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleChange = (e) => {
+    setEditedEntry(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
+  let input = "";
+
   return (
-    <StyledAttribute>
-      <span>{title}</span>
-      <input type="text" name={input} id="" placeholder={title} />
-    </StyledAttribute>
+    <StyledContainer>
+      <StyledTitle>{title}</StyledTitle>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedEntry}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          autoFocus
+        />
+      ) : (
+        <StyledEntry onDoubleClick={handleDoubleClick}>
+          {editedEntry}
+        </StyledEntry>
+      )}
+    </StyledContainer>
   );
-}
+};
+
+const StyledSpan = styled.span`
+  cursor: pointer;
+  width: 4rem;
+`;
+const StyledContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  input {
+    width: 4rem;
+    font-size: 2rem;
+  }
+`;
+const StyledTitle = styled.div`
+  text-transform: uppercase;
+  font-size: 0.5rem;
+`;
+const StyledEntry = styled.div`
+  text-transform: capitalize;
+  font-size: 2rem;
+`;
+
+export default ListItem;
