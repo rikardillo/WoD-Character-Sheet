cb0d042d59e1b1571748a9ef01950112e11beea8
Joan Gabriel Peralta Santana <joanperalta13@gmail.com>
2024-04-11 17:07:06 -0400

    chore: configure typescript

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Routes } from "./routes";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>
